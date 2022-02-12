253a0d63c0bb247971ff049f9c23ce99fa3836ba
Jordan Smith <jssmith9876@yahoo.com>
2022-02-12 15:04:45 -0800

    Nodes can be dragged and dropped

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const ctx = canvas.getContext('2d');
 
 // Node info
 const nodeFillStyle = "#22CCCC";
+const selectedNodeFillStyle = "#88aaaa";
 const nodeStrokeStyle = "#009999";
 const nodeRadius = 7;
 
@@ -15,49 +16,101 @@ const nodeRadius = 7;
  *  END CONSTANTS
  */
 
+// Globals
 let nodes = [];
+let currentSelected = undefined; 
 
+/*
+ *  BEGIN HELPER FUNCTIONS
+ */
+
+// Resizes the canvas dynamically to the window size when the page first laods
 const resizeCanvas = () => {
     canvas.width = 0.75 * window.innerWidth;
     canvas.height = 400;
 }
 
-const drawNode = (x, y) => {
-    ctx.beginPath();
-    ctx.fillStyle = nodeFillStyle;
-    ctx.arc(x, y, nodeRadius, 0, Math.PI * 2, true);
-    ctx.strokeStyle = nodeStrokeStyle;
-    ctx.stroke();
-    ctx.fill();
+// Gets the position of the mouse with respect to the canvas
+const getMousePosition = (mouseEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    return [mouseEvent.clientX - rect.left,
+            mouseEvent.clientY - rect.top];
 }
 
-// Canvas onclick handler
-canvas.addEventListener('mousedown', (event) => {
-    // Get the coordinates with respect to the canvas 
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+// Draws a node at the current position
+const drawNodes = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    let clickedOnANode = false;
-
-    // Check if the user clicked on a node
-    nodes.every(([node_x, node_y]) => {
-        const dx = Math.abs(node_x - x);
-        const dy = Math.abs(node_y - y);
-
-        if (Math.sqrt( (dx * dx) + (dy * dy)) <= nodeRadius) {
-            clickedOnANode = true;
-            return false;
-        }
-
-        return true;
+    nodes.forEach((node) => {
+        ctx.beginPath();
+        ctx.fillStyle = node.selected ? selectedNodeFillStyle : nodeFillStyle;
+        ctx.arc(node.x, node.y, nodeRadius, 0, Math.PI * 2, true);
+        ctx.strokeStyle = nodeStrokeStyle;
+        ctx.stroke();
+        ctx.fill();
     });
+}
 
-    if (clickedOnANode) {
-        
-    } else {
-        // Create the node
-        nodes.push([x, y]);
-        drawNode(x, y);
+// Checks if the current mouse position is clicking on a node
+const clickedOnNode = (mouseX, mouseY) => {
+    return nodes.find((node) => {
+        return mouseX > (node.x - nodeRadius) &&
+               mouseX < (node.x + nodeRadius) &&
+               mouseY > (node.y - nodeRadius) &&
+               mouseY < (node.y + nodeRadius);
+    });
+}
+
+/*
+ *  END HELPER FUNCTIONS
+ */
+
+/*
+ *  BEGIN EVENT HANDLERS
+ */
+
+canvas.addEventListener('mousemove', (event) => {
+    if (currentSelected) { 
+        const [mouseX, mouseY] = getMousePosition(event);
+        currentSelected.x = mouseX;
+        currentSelected.y = mouseY;
+        drawNodes();
     }
 });
+
+canvas.addEventListener('mousedown', (event) => {
+    const [mouseX, mouseY] = getMousePosition(event);
+
+    const clickedTarget = clickedOnNode(mouseX, mouseY);
+    if (currentSelected && currentSelected.selected) {
+        currentSelected.selected = false;
+    } 
+    if (clickedTarget) {
+        currentSelected = clickedTarget;
+        currentSelected.selected = true;
+        drawNodes();
+    }
+});
+
+canvas.addEventListener('mouseup', (event) => {
+    if (!currentSelected) {
+        const [mouseX, mouseY] = getMousePosition(event);
+
+        let newNode = {
+            x: mouseX,
+            y: mouseY,
+            selected: false
+        };
+        nodes.push(newNode);
+        drawNodes();
+    } 
+    if (currentSelected && currentSelected.selected) {
+        currentSelected.selected = false;
+        currentSelected = undefined;
+    }
+    drawNodes();
+});
+
+/*
+ *  END EVENT HANDLERS
+ */
